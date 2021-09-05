f812a6648f86536888069af0bdad4614839778d4
SiddhantKadam <siddhantk951@gmail.com>
2021-09-05 19:21:59 +0530

    Quiz App 05-09

--- quiz-app/src/app/constants/configUrl.ts
+++ quiz-app/src/app/constants/configUrl.ts
@@ -1,10 +1,8 @@
 export const MainURL = {
   
     //LOCAL SERVER
-    //  HostUrl: 'http://localhost:9944',
+     HostUrl: 'http://localhost:9988',
 
-    //Swagger
-    HostUrl : 'http://2ca0-2402-8100-3000-da16-5469-4134-7276-89ed.ngrok.io',
     
     imageUrl: '/assets'
   }
